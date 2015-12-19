eef6b8a1e0a7547d9cd4e80d6f5edf8bc1bf3453
Michael Pereverov <mpereverov@gmail.com>
2015-12-19 11:36:18 +0200

    clean inst Yii2 basic

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=blog1',
+    'username' => 'root',
+    'password' => 'qwerty',
+    'charset' => 'utf8',
+];
